7a4114fc48c719a16ecf26182caca2f50ab5ffc1
Matias Martinez <matimartinezz.927@gmail.com>
2024-10-01 12:27:43 -0300

    agrege skills

---
+++ src/const.js
@@ -0,0 +1,114 @@
+import Node from "./components/icons/Node.astro";
+import Tailwind from "./components/icons/Tailwind.astro";
+import Firebase from "./components/icons/Firebase.astro";
+import MongoDB from "./components/icons/MongoDb.astro";
+import HTML from "./components/icons/HTML.astro";
+import Css from "./components/icons/CSS.astro";
+import JavaScript from "./components/icons/JavaScript.astro";
+import React from "./components/icons/React.astro";
+import Git from "./components/icons/Git.astro";
+import Astro from "./components/icons/AstroIcon.astro";
+import MySql from "./components/icons/MySql.astro";
+import Express from "./components/icons/Express.astro";
+
+
+export const TAGS = {
+    HTML: {
+      name: "Html.js",
+      class: "bg-[#AC3A0F]",
+      icon: HTML,
+    },
+    CSS: {
+      name: "Css.js",
+      class: "bg-[#1A5F76]",
+      icon: Css,
+    },
+    JAVASCRIPT: {
+      name: "JavaScript",
+      class: "bg-[#5C4F00]",
+      icon: JavaScript,
+    },
+    REACT: {
+      name: "React",
+      class: "bg-blue-900",
+      icon: React,
+    },
+    NODE:{
+      name: "Node.js",
+      class: "bg-[#0C560C]",
+      icon: Node,
+    },
+    EXPRESS:{
+        name: "Express.js",
+        class: "bg-[#000000] text-white",
+        icon: Express,
+    },
+    MONGODB: {
+      name: "MongoDB",
+      class: "bg-[#8B4513]",
+      icon: MongoDB,
+    },
+    MYSQL: {
+      name: "MySql",
+      icon: MySql,
+    },
+    GIT: {
+      name: "Git",
+      icon: Git,
+    },
+    ASTRO: {
+      name: "Astro",
+      icon: Astro,
+    },
+    TAILWIND: {
+      name: "Tailwind CSS",
+      class: "bg-[#1A5F76]",
+      icon: Tailwind,
+    },
+  
+    FIREBASE: {
+      name: "Firebase",
+      class: "bg-[#AC3A0F]",
+      icon: Firebase,
+    },
+  };
+
+  export const PROJECTS = [
+    {
+      title: "Mateando Ando",
+      description:
+        "ecommerce sobre Mates, Bombillas, Termos y accesorios sobre mates.Es mi primer proyecto creado desde cero con React y Firebase.",
+      link: "https://mateando-ando.vercel.app/",
+      github: "https://github.com/Matiim/mateando_ando",
+      image: "/proyects/mateando-ando.webp",
+      tags: [TAGS.REACT, TAGS.FIREBASE],
+    },
+    {
+      title: "Tareas-CRUD",
+      description:
+        "CRUD de Tareas hecho con el framework MERN, con sistema de registro y login de usuarios completo.",
+      /* link: "#", */
+      github: "https://github.com/Matiim/Tareas-CRUD",
+      image: "/proyects/tasks-mern.webp",
+      tags: [ TAGS.REACT, TAGS.MONGODB,TAGS.NODE,TAGS.EXPRESS]
+    },
+    {
+      title: "Juego-QUIZ",
+      description:
+        "Juego QUIZ sensillo , con distintas categorias de preguntas y suma de puntos para ganar usando LocalState.",
+      link: "juego-quiz-murex.vercel.app",
+      github: "https://github.com/Matiim/juego-quiz",
+      image: "/proyects/quiz.webp",
+      tags: [TAGS.HTML,TAGS.CSS, TAGS.JAVASCRIPT ]
+    },
+    {
+      title: "Hype-Sneakers",
+      description:
+        "Ecommerce de zapatillas completo con sistema de pago, panel para el administrador para poder agregar ,editar o eliminar productos , y un sistema de registro y login de usuarios completo.",
+      /* link: "#", */
+      github: "https://github.com/Matiim/Hype-Sneakers",
+      image: "/proyects/tasks-mern.webp",
+      tags: [ TAGS.REACT, TAGS.MONGODB,TAGS.NODE,TAGS.EXPRESS]
+    },
+    
+  ];
